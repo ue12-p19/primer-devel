bdda912e1456857f348199ec5838fed43e2a8741
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-08-01 08:52:34 +0200

    fix tracks again

--- nbhosting/tracks.py
+++ nbhosting/tracks.py
@@ -17,7 +17,7 @@ def tracks(coursedir):
 
     track_specs = [
         ('primer' , 'Python primer', 
-         [ ('primer', 'notebooks/[0-9]-*.ipynb'),
+         [ ('primer', '[0-9]*.ipynb'),
          ]),
         ]
 
